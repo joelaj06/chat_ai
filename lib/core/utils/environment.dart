49d8adedc5fd61a39b6576613b35ef656340db18
Joel Acquah <joelaj06@gmail.com>
2023-04-13 03:56:46 +0000

    Removed key

--- lib/core/utils/environment.dart
+++ lib/core/utils/environment.dart
@@ -1,5 +1,5 @@
 String chatBaseUrl = 'https://api.openai.com/v1/';
-String apiKey = 'sk-IFBx5x56LU2YjLBOWk24T3BlbkFJqaHTQGzYTljQtEStsFzg';
+String apiKey = '';
 Map<String, String> headers = <String, String>{
   'Content-type': 'application/json',
   'Authorization' : 'Bearer $apiKey'
